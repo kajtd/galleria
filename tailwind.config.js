13ea02749518a4cbfcf767aefe1dfe09c5a82d1c
Kajetan Domagala <kajtekd8@gmail.com>
2023-11-12 16:12:42 +0100

    add custom variables in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,17 @@ export default {
   content: [],
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: '#7D7D7D',
+        'light-gray': '#E5E5E5',
+        'very-light-gray': '#F3F3F3',
+      },
+      backgroundImage: {
+        'image-art-background':
+          'linear-gradient(180deg, rgba(0, 0, 0, 0.0001) 0%, rgba(0, 0, 0, 0.841672) 100%)',
+      },
+    },
   },
   plugins: [],
-}
-
+};

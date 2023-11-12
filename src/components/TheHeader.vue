e76bc6443efd4061adf9aee3ebb2d39e775fc4cf
Kajetan Domagala <kajtekd8@gmail.com>
2023-11-12 16:13:43 +0100

    add header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header
+    class="max-w-[1360px] mx-auto w-full flex justify-between items-center border-b border-light-gray pb-6 md:pb-10 mb-3 md:mb-5"
+  >
+    <img
+      src="./../../public/assets/shared/logo.svg"
+      alt=""
+      class="w-[113px] md:w-[169px]"
+    />
+    <button
+      class="font-bold text-[9px] md:text-xs uppercase text-gray tracking-[1.93px] md:tracking-[2.57px]"
+    >
+      Start Slideshow
+    </button>
+  </header>
+</template>

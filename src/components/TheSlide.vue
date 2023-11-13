d05a218f2a6fdb7fdd3312d74372a9b73be0d2b6
Kajetan Domagala <kajtekd8@gmail.com>
2023-11-13 15:35:27 +0100

    change defineProps structure in TheSlide.vue

--- src/components/TheSlide.vue
+++ src/components/TheSlide.vue
@@ -4,6 +4,12 @@
   import { Image } from '../types/image';
   import ImageModal from './ImageModal.vue';
 
+  type Props = {
+    slide: Slide;
+  };
+
+  defineProps<Props>();
+
   const image = ref<Image>({
     url: '',
     alt: '',
@@ -22,15 +28,6 @@
     isImageModalVisible.value = false;
     document.querySelector('body')?.classList.remove('modal');
   };
-
-  withDefaults(
-    defineProps<{
-      slide: Slide | null;
-    }>(),
-    {
-      slide: null,
-    }
-  );
 </script>
 
 <template>
@@ -40,12 +37,12 @@
     <div class="relative">
       <img
         class="w-[327px] h-[280px] md:w-[475px] md:h-[560px] object-cover"
-        :src="slide?.images.gallery"
-        :alt="slide?.name"
+        :src="slide.images.gallery"
+        :alt="slide.name"
       />
       <button
         class="absolute top-4 left-4 md:bottom-4 md:top-auto xl:left-4 xl:bottom-24 bg-[rgba(0,0,0,0.75)] text-white flex items-center py-[14px] px-4 gap-[14px]"
-        @click="viewImage(slide?.images.gallery, slide?.name)"
+        @click="viewImage(slide.images.gallery, slide.name)"
       >
         <img
           src="./../../public/assets/shared/icon-view-image.svg"
@@ -59,14 +56,14 @@
       class="bg-white pt-6 relative bottom-16 right-8 pl-12 w-[280px] h-[133px] md:w-[445px] md:h-[302px] md:absolute md:top-0 xl:relative xl:right-16"
     >
       <h1 class="text-2xl md:text-[56px] md:leading-[64px] font-bold">
-        {{ slide?.name }}
+        {{ slide.name }}
       </h1>
-      <h2 class="text-[15px] text-gray mt-6">{{ slide?.artist.name }}</h2>
+      <h2 class="text-[15px] text-gray mt-6">{{ slide.artist.name }}</h2>
     </div>
     <img
       class="w-16 h-16 md:w-[128px] md:h-[128px] relative bottom-10 left-4 md:absolute md:top-[300px] md:left-[546px] xl:left-[550px] xl:top-[500px]"
-      :src="slide?.artist.image"
-      :alt="slide?.artist.name"
+      :src="slide.artist.image"
+      :alt="slide.artist.name"
     />
     <div
       class="flex flex-col max-w-full xl:max-w-[350px] relative md:mt-16 md:max-w-[457px] md:mx-auto"
@@ -74,16 +71,16 @@
       <div
         class="text-[100px] md:text-[200px] text-very-light-gray font-bold z-[-1] absolute -top-20 -right-2 md:-left-20 xl:-top-36"
       >
-        {{ slide?.year }}
+        {{ slide.year }}
       </div>
       <p
         class="leading-[28px] text-gray font-bold mt-6 max-w-[457px] mx-auto md:mt-16 xl:relative xl:right-16 xl:bottom-6"
       >
-        {{ slide?.description }}
+        {{ slide.description }}
       </p>
       <a
         class="uppercase underline mt-16 md:mt-20 tracking-[1.93px] text-[9px] font-bold text-gray xl:relative xl:right-16"
-        :href="slide?.source"
+        :href="slide.source"
         target="_blank"
       >
         Go to source
